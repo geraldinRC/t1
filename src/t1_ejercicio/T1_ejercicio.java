2980dad2617110576a7f4e5f4e9321492226bdab
geraldinRC <142689847+geraldinRC@users.noreply.github.com>
2023-09-09 13:58:40 -0500

    Add files via upload

---
+++ src/t1_ejercicio/T1_ejercicio.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t1_ejercicio;
+
+/**
+ *
+ * @author usuario
+ */
+public class T1_ejercicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      framelista pantalla = new framelista ();
+      pantalla.setLocationRelativeTo(null);
+      pantalla.setVisible(true);
+    }
+    
+}
---
+++ src/t1_ejercicio/persona.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t1_ejercicio;
+
+/**
+ *
+ * @author usuario
+ */
+public class persona {
+    
+}
